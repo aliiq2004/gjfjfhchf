25033f163c6e14e5453068c62444bbebb6061604
aliiq2004 <60258849+aliiq2004@users.noreply.github.com>
2022-06-20 23:21:15 +0300

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,24 @@
+sudo apt install python3.9
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.9
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
+sudo update-alternatives --config python3
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.9
+apt install python3.9-distutils
+sudo apt install python3.9-distutils
+pip3 install --upgrade setuptools
+pip3 install --upgrade pip
+pip3 install --upgrade distlib
+pip3 install telethon
+pip3 install redis
+pip3 install python-dotenv==0.20.0
+sudo apt install redis -y
+pip3 install telegram
+pip3 install python-telegram-bot
+clear
+redis-server --daemonize yes
+sudo screen python3 bot.py
