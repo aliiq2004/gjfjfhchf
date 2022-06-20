c0148b7acd00c1b7f060681e0aff0d3d4d10658d
aliiq2004 <60258849+aliiq2004@users.noreply.github.com>
2022-06-20 23:43:06 +0300

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,12 @@
+pip3 install --upgrade setuptools
+pip3 install --upgrade pip
+pip3 install --upgrade distlib
+pip3 install telethon
+pip3 install redis
+pip3 install python-dotenv==0.20.0
+sudo apt install redis -y
+pip3 install telegram
+pip3 install python-telegram-bot
+clear
+redis-server --daemonize yes
+python3 bot.py
